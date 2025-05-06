6d61935f9b6374bb56743da3c73bcc09bc437b60
fernandatc17 <abigailticlavilca46@gmail.com>
2025-05-06 18:06:22 -0500

    cambio´para vistas desde navegador

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 
 sequelize.sync({ force: false }).then(() => {
   console.log('Base de datos sincronizada');
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
   });
 });
